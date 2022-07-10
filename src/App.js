29eb79e800ab6aa3ebc8b42466f74fb347a655b8
SAgamyradov <agamyradowserdar777@gmail.com>
2022-07-10 09:18:00 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1 className="text-3xl underline font-montserratAlternates-bold">
+      Hello world!
+      
+      <h1 className="text-3xl underline font-montserratAlternates-medium">
+      Hello world!
+      
+    </h1>
+
+    <h1 className="text-3xl underline font-montserrat-bold">
+      Hello world!
+      
+    </h1>
+
+    <h1 className="text-3xl underline font-montserrat-medium">
+      Hello world!
+      
+    </h1>
+    </h1>
+    
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        'montserrat-bold':["Montserrat-Bold"],
+        'montserrat-medium':["Montserrat-Medium"],
+        'montserratAlternates-bold':["MontserratAlternates-Bold"],
+        'montserratAlternates-medium':["MontserratAlternates-Medium"]
+      }
+    },
+  },
+  plugins: [],
+}
