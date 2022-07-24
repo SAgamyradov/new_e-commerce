705464c3040618980938ea47e96cfeb78cbd240a
SAgamyradov <agamyradowserdar777@gmail.com>
2022-07-24 04:01:16 -0400

    scale

--- src/components/Sidebar/Navigation.jsx
+++ src/components/Sidebar/Navigation.jsx
@@ -5,7 +5,7 @@ import
       {BsDoorClosed, 
        BsFillDoorOpenFill,
        BsBasket2,
-       BsFillSuitHeartFill}
+                  }
         from "react-icons/bs"
 import {AiOutlineShoppingCart} from 'react-icons/ai'
 import { NavLink } from 'react-router-dom'
@@ -20,42 +20,40 @@ const Navigation = () => {
     <div className={`${open ? 'w-0' : 'w-64'} lg:block duration-700 h-screen bg-white `}>
       <CgController size={25} className={`${!open && "rotate-180 duration-300"} md:hidden lg:hidden absolute cursor-pointer   left-2 w-7 top-3 `} 
         onClick={toggle} />
-        <img alt='' src={logoImage} className='mt-16 md:w-36 h-16  mx-9 md:mt-7'/>
+        <img alt='' src={logoImage} className={`mt-16 md:w-36 h-16  mx-9 md:mt-7 ${open && "scale-0"}`}/>
       
       <NavLink to={'/'}
         className={` flex items-center  mx-4 mt-7 p-3 w-60 font-montserrat-medium 
-         hover:bg-yellow-200  hover:text-slate-700 text-yellow-800 rounded-l-full duration-500`}>
+         hover:bg-yellow-200  hover:text-slate-700 text-yellow-800 rounded-l-full duration-500 ${open && "scale-0"}`}>
           <FaHome className='mx-1'/>
           Bas sahypa
       </NavLink>
 
-      <NavLink to={'/'} className=' flex items-center  mx-4 mt-1 p-3 w-60 font-montserrat-medium
-       hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500'>
+      <NavLink to={'/'} 
+      className={` flex items-center  mx-4 mt-1 p-3 w-60 font-montserrat-medium
+       hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500 ${open && "scale-0"}`}>
         <BsDoorClosed className='mx-1'/>
       Agac gapy
       </NavLink>
 
-      <NavLink to={'/'} className='flex items-center  mx-4 mt-1 p-3 w-60 font-montserrat-medium 
-        hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500'>
+      <NavLink to={'/'} className={`flex items-center  mx-4 mt-1 p-3 w-60 font-montserrat-medium 
+        hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500 ${open && "scale-0"}`}>
         <BsFillDoorOpenFill className='mx-1'/>  
         Demir gapy
       </NavLink>
 
-      <NavLink to={'/'} className='flex items-center mx-4 mt-1 p-3 w-60 font-montserrat-medium
-      hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500'>
+      <NavLink to={'/'} className={`flex items-center mx-4 mt-1 p-3 w-60 font-montserrat-medium
+      hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500 ${open && "scale-0"}`}>
         <AiOutlineShoppingCart className='mx-1'/>
         Magazynlar
       </NavLink>
      
-     <NavLink to={'/'} className='flex items-center mx-4 mt-1 p-3 w-60 font-montserrat-medium 
-     hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500'>
+     <NavLink to={'/'} className={`flex items-center mx-4 mt-1 p-3 w-60 font-montserrat-medium 
+     hover:bg-yellow-200 hover:text-slate-700 text-yellow-800 rounded-l-full duration-500 ${open && "scale-0"}`}>
       <BsBasket2 className='mx-1'/>
       Sebedim
      </NavLink>
-       <div className='flex items-center mt-64 mx-11 font-montserrat-medium relative'>
-     <BsFillSuitHeartFill className='text-red-600 mx-1'/>
-     OGUZLY 2022
-        </div>
+      
     </div>
     </div>
   )
